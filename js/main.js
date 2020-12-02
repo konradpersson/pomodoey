eaa44b450ce533b2070a013faab241064dce51a9
Konrad Persson <konrad.persson@gmail.com>
2020-12-02 14:49:24 +0100

    Drawing new tasks
    Adding new tasks to the markup after submitting them

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const newItemForm = document.querySelector(".new-item-form");
 const newItemButton = document.querySelector(".new-item-form button");
+const todoList = document.querySelector(".todo-list");
 
 let items = [];
 
@@ -18,21 +19,23 @@ newItemForm.addEventListener("input", function(e) {
 
 
 newItemForm.addEventListener("submit", function(e) {
+
     e.preventDefault();
 
+    // I dont get this - How do I select a specific name which is not name
     const input = this.name;
     const inputVal = input.value;
 
     if (inputVal != "") {
         const item = {
-            id: Date().getTime(),
+            id: new Date().getTime(),
             name: inputVal,
             complete: false
         };
 
         items.push(item);
 
-        localStorage.setItem("items", JSON.stringify(tasks));
+        localStorage.setItem("items", JSON.stringify(items));
 
         drawItem(item);
 
@@ -49,9 +52,12 @@ function drawItem(item) {
     itemWrapper.setAttribute("id", itemWrapper.id);
 
     const itemContent = `
-        <input type="checkbox" id="todo-${item}.id">
+        <input type="checkbox" id="todo-${item.id}">
         <label for="todo-${item.id}">${item.name}</label>
         <button aria-label="delete todo-${item.id}">&times;</button>
     `;
 
-}
+    itemWrapper.innerHTML = itemContent;
+    todoList.appendChild(itemWrapper);
+
+};
